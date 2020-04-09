07f5f108ae0d9bf3d0df86b3c33855423e9e2ae3
PratyushMoorthy <pratyush.v.m@gmail.com>
2020-04-09 16:28:04 +0530

    Update graph.cuh

--- include/graph.cuh
+++ include/graph.cuh
@@ -1,33 +1,8 @@
 #ifndef __GRAPH__HEADER__CUDA__
-#define __GRAPH__HEADER__CUDA
+#define __GRAPH__HEADER__CUDA__
 
-//#include<stdio.h>
 #include<cuda.h>
-//#include<utility>
 #include"graph.h"
-
-/*#define pii std::pair<int,int>
-#define mp std::make_pair
-#define f first
-#define s second
-
-//declaration of class object - graph
-
-class graph
-{
-public:
-    
-    int v,e; //number of vertices and edges
-    int *depth; //array that stores depth (or) distance of each vertex from source
-    pii *edgelist; //list of edges in the form of (vertex1,vertex2) pairs  
-
-};
-
-//function prototypes for host functions
-
-void simple_bfs(graph *cpu_g, graph *gpu_g, bool *cpu_done, bool *gpu_done);
-void readgraph(pii *c_edgelist, int nv, int ne, int argc, char **argv);
-void printgraph(graph *cpu_g);*/
 
 //function prototypes for kernels 
 
